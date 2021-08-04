9aba2fffb93a41be4bdb92f2e5c09d7dc9aa14c0
Thomas Luff <tomfluff@gmail.com>
2021-08-04 10:19:30 +1000

    Flake8 formatting fixes

--- monitor.py
+++ monitor.py
@@ -14,31 +14,34 @@ TEMPERATURE_GAUGE = Gauge('temperature', 'Temperature')
 HUMIDITY_GAUGE = Gauge('humidity', 'Humidity')
 SENSOR_ERRORS = Counter('sensor_errors', 'Errors reading the sensor')
 
+
 @SENSOR_TIME.time()
 def read_sensor():
     return Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN, 30, 1)
 
+
 def main_loop(loop_delay):
-  while True:
-    timestamp = datetime.now().isoformat()
-    humidity, temperature = read_sensor()
+    while True:
+        timestamp = datetime.now().isoformat()
+        humidity, temperature = read_sensor()
 
-    if humidity is not None and temperature is not None:
-      HUMIDITY_GAUGE.set(humidity)
-      TEMPERATURE_GAUGE.set(temperature)
-      print(f"{timestamp} Temp: {temperature:.1f}*C  Humidity: {humidity:.1f}%")
-    else:
-      SENSOR_ERRORS.inc()
-      print("Failed to retrieve data from humidity sensor")
+        if humidity is not None and temperature is not None:
+            HUMIDITY_GAUGE.set(humidity)
+            TEMPERATURE_GAUGE.set(temperature)
+            print(f"{timestamp} Temp: {temperature:.1f}*C  Humidity: {humidity:.1f}%")
+        else:
+            SENSOR_ERRORS.inc()
+            print("Failed to retrieve data from humidity sensor")
 
-    time.sleep(loop_delay)
+        time.sleep(loop_delay)
+
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--metrics-port', default=8000, type=int)
-  parser.add_argument('--loop-delay', default=5, type=int)
-  args = parser.parse_args()
-  start_http_server(args.metrics_port)
-  print(f'Started metrics server on :{args.metrics_port}')
-  print(f'Reading sensor every {args.loop_delay} seconds')
-  main_loop(args.loop_delay)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--metrics-port', default=8000, type=int)
+    parser.add_argument('--loop-delay', default=5, type=int)
+    args = parser.parse_args()
+    start_http_server(args.metrics_port)
+    print(f'Started metrics server on :{args.metrics_port}')
+    print(f'Reading sensor every {args.loop_delay} seconds')
+    main_loop(args.loop_delay)
